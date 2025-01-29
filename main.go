b656cd18e600d8e03f80a14e56a0906ce1bcf785
Bantu-art <brianbaraza40@gmail.com>
2025-01-29 22:03:13 +0300

    feat: register route for created posts page
    - Added route handler for  using
    - Enables serving the created posts page through the defined controller

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	http.HandleFunc("/signin", handlers.SignInHandler)
 	// Add other route handlers...
 	// http.Handle("/", &controllers.PostHandler{})
-	http.HandleFunc("createdpost", controllers.CreatedPost)
+	http.HandleFunc("templates/createdpost", controllers.CreatedPost)
 
 	postHandler := controllers.NewPostHandler()
 	http.Handle("/", postHandler)
