190f8a263037a9ff1cab41537aa59e9144e155f3
bshisia <shisiabrian7@gmail.com>
2025-02-10 14:11:32 +0300

    refactor: refactor the status checker from the categories file

---
+++ controllers/validatestatus.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"net/http"
+
+	"forum/utils"
+)
+
+func (ch *CategoryHandler) checkAuthStatus(r *http.Request) bool {
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		return false
+	}
+	_, err = utils.ValidateSession(utils.GlobalDB, cookie.Value)
+	return err == nil
+}
